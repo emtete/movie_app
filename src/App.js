3a2acc06628de3f0ee79f23aeaf6e22086a832a9
최종호 <victor_77@naver.comgit config --global user.name emtete>
2020-06-10 11:50:14 +0900

    Change component class to function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-    };
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isLoading: true,
+//     };
+//   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
-  }
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ isLoading: false });
+//     }, 6000);
+//   }
 
-  render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
-  }
+//   render() {
+//     const { isLoading } = this.state;
+//     return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+//   }
+// }
+function App(props) {
+  const [name, setName] = useState("");
+  // const [isLoading, setIsLoading] = useState('');
+  // return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+
+  setTimeout(() => {
+    setName("abc");
+  }, 2000);
+
+  return <div>{`hello, my name is ${name}`}</div>;
 }
 
 export default App;
--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -1,12 +1,13 @@
 // import sinon from "sinon";
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import App from "../App";
 
 it("Check state when setTimeout(before, after)", () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.state().isLoading).toEqual(true);
-  setTimeout(() => {
-    expect(wrapper.state().isLoading).toEqual(false);
-  }, 6000);
+  // expect(wrapper.state().isLoading).toEqual(true);
+  // setTimeout(() => {
+  //   expect(wrapper.state().isLoading).toEqual(false);
+  // }, 6000);
+  expect(wrapper.state().name).toEqual("");
 });
