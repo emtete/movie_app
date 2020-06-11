2fcfaf8c7efec1f7dd70e19679f9aa11c35b4d5d
최종호 <victor_77@naver.comgit config --global user.name emtete>
2020-06-12 00:09:00 +0900

    (TEST) apply componentDidMount() but not async

--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -20,15 +20,37 @@ jest.setTimeout(30000);
 
 // function dummy() {}
 
-describe("Check getMovie", () => {
-  let movies = [];
+// describe("Check getMovie", () => {
+//   let movies = [];
 
-  beforeEach(async () => {
-    const result = await axios.get("https://yts.mx/api/v2/list_movies.json");
-    movies = result.data.data.movies;
+//   beforeEach(async () => {
+//     const result = await axios.get("https://yts.mx/api/v2/list_movies.json");
+//     movies = result.data.data.movies;
+//   });
+
+//   it("ComponentDidMount", () => {
+//     expect(movies).not.toBeUndefined();
+//   });
+// });
+
+describe("Check ComponentDidMount", () => {
+  // it("Check", () => {
+  //   const spy = jest.spyOn(App.instance, "getMovies");
+  //   const wrapper = mount(<App />);
+  //   wrapper.instance().getMovies();
+  //   expect(spy).toHaveBeenCalled();
+  // });
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
   });
 
-  it("ComponentDidMount", () => {
-    expect(movies).not.toBeUndefined();
+  it("should check `componentDidMount()`", () => {
+    const instance = wrapper.instance(); // you assign your instance of the wrapper
+    jest.spyOn(instance, "getMovies"); // You spy on the randomFunction
+    instance.componentDidMount();
+    expect(instance.getMovies).toHaveBeenCalledTimes(1); // You check if the condition you want to match is correct.
+    console.log(instance.state.isLoading);
   });
 });
