315dee6486baba7ceba39f193f7ea84e7648984e
최종호 <victor_77@naver.comgit config --global user.name emtete>
2020-06-11 23:55:26 +0900

    (TEST) Apply beforeEach and axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
+      movies: [],
     };
   }
 
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    this.setState({ movies, isLoading: false });
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -1,19 +1,34 @@
 ("use strict");
 // import sinon from "sinon";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { shallow, mount } from "enzyme";
 import App from "../App";
+import sinon from "sinon";
+import axios from "axios";
 
 jest.setTimeout(30000);
 
-it("Check state when setTimeout(before, after)", (dummy) => {
-  const wrapper = shallow(<App />);
+// it("Check state when setTimeout(before, after)", (dummy) => {
+//   const wrapper = shallow(<App />);
 
-  expect(wrapper.state().isLoading).toEqual(true);
-  setTimeout(() => {
-    expect(wrapper.state().isLoading).toEqual(false);
-    dummy();
-  }, 7000);
+//   expect(wrapper.state().isLoading).toEqual(true);
+//   setTimeout(() => {
+//     expect(wrapper.state().isLoading).toEqual(false);
+//     dummy();
+//   }, 7000);
+// });
+
+// function dummy() {}
+
+describe("Check getMovie", () => {
+  let movies = [];
+
+  beforeEach(async () => {
+    const result = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    movies = result.data.data.movies;
+  });
+
+  it("ComponentDidMount", () => {
+    expect(movies).not.toBeUndefined();
+  });
 });
-
-function dummy() {}
