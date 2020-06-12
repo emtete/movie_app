80a310cac416dcee6ca79253a9ee7f6ae5158ca8
최종호 <victor_77@naver.comgit config --global user.name emtete>
2020-06-12 10:41:39 +0900

    (TEST) clean test

--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -6,52 +6,32 @@ import App from "../App";
 import sinon from "sinon";
 import axios from "axios";
 
-jest.setTimeout(30000);
+describe("Check getMovie", () => {
+  let movies = [];
 
-// it("Check state when setTimeout(before, after)", (dummy) => {
-//   const wrapper = shallow(<App />);
+  beforeEach(async () => {
+    const result = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    movies = result.data.data.movies;
+  });
 
-//   expect(wrapper.state().isLoading).toEqual(true);
-//   setTimeout(() => {
-//     expect(wrapper.state().isLoading).toEqual(false);
-//     dummy();
-//   }, 7000);
-// });
-
-// function dummy() {}
-
-// describe("Check getMovie", () => {
-//   let movies = [];
-
-//   beforeEach(async () => {
-//     const result = await axios.get("https://yts.mx/api/v2/list_movies.json");
-//     movies = result.data.data.movies;
-//   });
-
-//   it("ComponentDidMount", () => {
-//     expect(movies).not.toBeUndefined();
-//   });
-// });
+  it("ComponentDidMount", () => {
+    expect(movies).not.toBeUndefined();
+  });
+});
 
 describe("Check ComponentDidMount", () => {
-  // it("Check", () => {
-  //   const spy = jest.spyOn(App.instance, "getMovies");
-  //   const wrapper = mount(<App />);
-  //   wrapper.instance().getMovies();
-  //   expect(spy).toHaveBeenCalled();
-  // });
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(<App />);
   });
 
-  it("should check `componentDidMount()`", async () => {
-    const instance = wrapper.instance(); // you assign your instance of the wrapper
-    jest.spyOn(instance, "getMovies"); // You spy on the randomFunction
+  it("componentDidMount", async () => {
+    const instance = wrapper.instance();
+    jest.spyOn(instance, "getMovies");
     instance.componentDidMount();
     await instance.getMovies();
-    expect(instance.getMovies).toHaveBeenCalledTimes(2); // You check if the condition you want to match is correct.
+    expect(instance.getMovies).toHaveBeenCalledTimes(2);
     expect(instance.state.isLoading).toEqual(false);
   });
 });
